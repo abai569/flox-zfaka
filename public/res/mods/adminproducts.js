327da759b5a9f984513c43790eb73d595b55a914
abai569 <abaibubaix@gmail.com>
2026-08-07 22:54:59 +0800

    完善商品图片管理界面

--- public/res/mods/adminproducts.js
+++ public/res/mods/adminproducts.js
@@ -126,72 +126,70 @@ layui.define(['layer', 'table', 'form','layedit','upload'], function(exports){
     });
 	
 	
-	//普通图片上传
-	var uploadInst = upload.render({
-		elem: '#upload'
-		,accept:'images'
-		,acceptMime: 'image/*'
-		,size:5120
-		,auto:false
-		,before: function(obj){
-			//预读本地文件示例，不支持ie8
-			obj.preview(function(index, file, result){
-				//$('#btnbox').show();
-				$("#btnbox").attr('disabled',false);
-				$("#btnbox").removeClass("layui-btn-disabled");
-				$('#upload').hide();
-				$('#preview').attr('src', result); //图片链接（base64）
-			});
+	var imageFiles = [];
+	$('#select_product_images').on('click', function(){ $('#product_image_files').click(); });
+	$('#product_image_files').on('change', function(){
+		imageFiles = Array.prototype.slice.call(this.files || []);
+		var oversized = imageFiles.some(function(file){ return file.size > 5 * 1024 * 1024; });
+		var invalidType = imageFiles.some(function(file){ return ['image/jpeg','image/png','image/gif','image/webp'].indexOf(file.type) === -1; });
+		var tooMany = imageFiles.length + Number($('#product_image_pid').data('image-count') || 0) > 10;
+		if (oversized || invalidType || tooMany) {
+			imageFiles = [];
+			this.value = '';
+			var message = oversized ? '单张图片不能超过5MB' : (invalidType ? '仅支持 JPG、PNG、GIF 和 WebP 图片' : '每个商品最多上传10张图片');
+			layer.msg(message, {icon:2, time:5000});
 		}
-		,choose: function(obj){
-			//预读本地文件示例，不支持ie8
-			obj.preview(function(index, file, result){
-				//$('#btnbox').show();
-				$("#btnbox").attr('disabled',false);
-				$("#btnbox").removeClass("layui-btn-disabled");
-				$('#upload').hide();
-				$('#preview').attr('src', result); //图片链接（base64）
-			});
-		}
-	});	
-	//上传
-	form.on('submit(upload)', function(data){
-		data.field.csrf_token = TOKEN;
-		var i = layer.load(2,{shade: [0.5,'#fff']});
-		var formData = new FormData(document.getElementById("products_imgurl_form"));
-		$.ajax({
-			url: '/'+ADMIN_DIR+'/products/imgurlajax',
-			type: 'POST',
-			dataType: 'json',
-			data:formData,
-			processData: false,
-            contentType: false,
-		})
-		.done(function(res) {
-			if (res.code == '1') {
-				layer.open({
-					title: '提示',
-					content: '上传成功',
-					btn: ['确定'],
-					yes: function(index, layero){
-					    location.reload();
-					},
-					cancel: function(){ 
-					    location.reload();
-					}
-				});
-			} else {
-				layer.msg(res.msg,{icon:2,time:5000});
-			}
-		})
-		.fail(function() {
-			layer.msg('服务器连接失败，请联系管理员',{icon:2,time:5000});
-		})
-		.always(function() {
-			layer.close(i);
-		});
+		$('#product_image_queue').text(imageFiles.length ? '已选择 '+imageFiles.length+' 张图片' : '');
+		$('#upload_product_images').prop('disabled', !imageFiles.length).toggleClass('layui-btn-disabled', !imageFiles.length);
+	});
 
-		return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
+	$('#upload_product_images').on('click', function(){
+		if (!imageFiles.length) return;
+		var pid = $('#product_image_pid').val();
+		var loading = layer.load(2,{shade:[0.5,'#fff']});
+		$('#select_product_images, #upload_product_images').prop('disabled', true).addClass('layui-btn-disabled');
+		var index = 0;
+		function finishUpload(message){
+			layer.close(loading);
+			$('#select_product_images').prop('disabled', false).removeClass('layui-btn-disabled');
+			$('#upload_product_images').prop('disabled', !imageFiles.length).toggleClass('layui-btn-disabled', !imageFiles.length);
+			if (message) layer.msg(message,{icon:2,time:5000});
+		}
+		function uploadNext(){
+			if (index >= imageFiles.length) {
+				finishUpload();
+				location.reload();
+				return;
+			}
+			$('#product_image_queue').text('正在上传 '+(index+1)+' / '+imageFiles.length);
+			var formData = new FormData();
+			formData.append('pid', pid);
+			formData.append('csrf_token', TOKEN);
+			formData.append('file', imageFiles[index]);
+			$.ajax({url:'/'+ADMIN_DIR+'/productimages/upload', type:'POST', dataType:'json', data:formData, processData:false, contentType:false})
+			.done(function(res){
+				if (res.code == 1) { index++; uploadNext(); }
+				else { finishUpload(res.msg); }
+			})
+			.fail(function(xhr){
+				finishUpload(xhr.status === 413 ? '单张图片不能超过5MB' : '图片上传失败，请稍后重试');
+			});
+		}
+		uploadNext();
+	});
+
+	function imageAction(action, id, extra){
+		var data = $.extend({pid:$('#product_image_pid').val(), id:id, csrf_token:TOKEN}, extra || {});
+		$.post('/'+ADMIN_DIR+'/productimages/'+action, data, function(res){
+			if (res.code == 1) location.reload();
+			else layer.msg(res.msg,{icon:2,time:5000});
+		}, 'json').fail(function(){ layer.msg('操作失败，请稍后重试',{icon:2,time:5000}); });
+	}
+	$('#product_image_grid').on('click', '.set-primary', function(){ imageAction('primary', $(this).closest('.product-image-tile').data('id')); });
+	$('#product_image_grid').on('click', '.move-image', function(){ imageAction('move', $(this).closest('.product-image-tile').data('id'), {direction:$(this).data('direction')}); });
+	$('#product_image_grid').on('click', '.delete-image', function(){
+		var id = $(this).closest('.product-image-tile').data('id');
+		layer.confirm('确定删除这张图片吗？', function(){ imageAction('delete', id); });
 	});
 	
 	exports('adminproducts',null)
