78577d4a03f59eb0b513acc8224fcb3561000152
abai569 <abaibubaix@gmail.com>
2026-08-07 17:22:42 +0800

    发布 v1.5.2 修复后台路由并增加 CI 容器烟测
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- application/init.php
+++ application/init.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('Asia/Chongqing');
 define('ENV', strtoupper(ini_get('yaf.environ')));
-define('VERSION', getenv('ZFAKA_VERSION') ?: '1.5.1');
+define('VERSION', getenv('ZFAKA_VERSION') ?: '1.5.2');
 define('WEB_NAME',"ZFAKA平台");
 define('URL_KEY', 'faG2#@@1');
 define('CUR_DATE', date('Y-m-d'));
